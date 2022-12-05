f9c15c3c6faa4d8c5dbcd18e94d33123a9d7d479
charlybarrio <5757@holbertonstudents.com>
2022-12-05 14:05:04 -0800

    main.h update

--- shell1/main.h
+++ shell1/main.h
@@ -1,9 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define PROMPT "$ "
+#define PROPERTY_SEPARATROR '='
+#define PATH_SEPARATROR ':'
+#define COMMAND_LINE_SEPARATROR "\n\t\r "
+#define END_STRING_CHAR '\0'
+extern char **environ;
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+int exit_fun(void);
 void execmd(char **argv);
+char *_getenv(const char *name);
+char *get_location(char *command);
+void _env(char **envir, char **arr);
+int main(int ac, char **argv);
+
 #endif
