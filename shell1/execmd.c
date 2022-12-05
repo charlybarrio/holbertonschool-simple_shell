cd682b7099c8ff09d97074f5c78c4aeb2c718a2c
charlybarrio <5757@holbertonstudents.com>
2022-12-05 14:12:22 -0800

    execmd update

--- shell1/execmd.c
+++ shell1/execmd.c
@@ -1,18 +1,30 @@
 #include "main.h"
-
-
 void execmd(char **argv)
-{
-	char *command = NULL;
+{		/**wpid is just here to catch the stack before its destroyed*/
+	pid_t pid;
+        char *command = NULL;
+	/**This var will check if child is finished to call exit*/
+	int status;
 
-	if (argv)
+	/**>Fork the function first and save the PID so the parent can wait*/
+	pid = fork();
+	command = argv[0]; /**>This is always true */
+
+	/** Remember fork returns twice, 1 for parent, 0 for child*/
+	if (pid == 0)
 	{
-		command = argv[0];
-
-		if (execve(command, argv, NULL) == -1)
-		{
-			perror("Error:");
-		}
+		if (execvp(command, argv) == -1)
+			perror("42 Error: ");
+		exit(EXIT_FAILURE);
 	}
-
+	else if (pid < 0) /** If <0, something went wrong forking*/
+		perror("43 Error: ");
+	else /** if it isnt 0 or -1 then the parent waits for child to die*/
+	{ /** use a do here so it runs once before checking */
+		do {
+			pid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			/** Use the wifexited and wifsignaled to check if child
+			was exited or cancelled. so parent can stop waiting*/
+	}
 }
