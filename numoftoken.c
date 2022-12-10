594f3275508da2b4f9bcbbeb7abf1c00a51feaa7
Elias Martin <5715@holbertonstudents.com>
2022-12-10 05:36:33 -0800

    betty

--- numoftoken.c
+++ numoftoken.c
@@ -1,23 +1,24 @@
 #include"main.h"
 /**
-  *
-  *
-  *
+  * numoftoken - number of element tokenizer.
+  * @lineptr: line read.
+  * @delim: delimitador para tokenizar.
+  * Return: number of token.
   */
-int numoftoken(char * lineptr,const char *delim)
+int numoftoken(char *lineptr, const char *delim)
 {
- char *token;
- int numt;
+	char *token;
+	int numt;
 
- token = strtok(lineptr, delim);
- 
- while (token != NULL)
- {
-	 numt++;
+token = strtok(lineptr, delim);
+
+while (token != NULL)
+{
+numt++;
 token = strtok(NULL, delim);
- }
- numt++;
- free(token);
- return (numt);
+}
+numt++;
+free(token);
+return (numt);
 }
 
